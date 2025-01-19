8758a4cafa8a53e3e0285e58ca636c20ffcf9c88
Liam <liam.elliott.nz@gmail.com>
2025-01-19 17:52:33 +1300

    Basic character movement
    Also you need to use godot 4.3

---
+++ scripts/player.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+var max_velocity:float = 500.0
+var velocity:Vector2 = Vector2(0.0, 0.0)
+var acceleration:float = 2000.0
+var deceleration:float = 3000.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+
+func _physics_process(delta:float) -> void:
+	
+	## Get vector of displacement direction
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * max_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+		
+	position += velocity * delta
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
